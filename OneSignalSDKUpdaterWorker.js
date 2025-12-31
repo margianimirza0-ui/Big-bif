2ca56af86fce06d629ac915bc7485c5179220d72
margianimirza0-ui <margianimirza0@gmail.com>
2025-12-31 04:39:45 +0400

    Update OneSignalSDKUpdaterWorker.js

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,1 +1,7 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+// ეხმარება ბრაუზერს შეტყობინების მიღების დაფიქსირებაში
+self.addEventListener('push', function(event) {
+    console.log('Push message received.');
+});
+
