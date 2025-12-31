c49be5b0fe0c30c9b184b2d4e113b5eb1b9f0764
margianimirza0-ui <margianimirza0@gmail.com>
2025-12-31 04:29:24 +0400

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,6 @@
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+// ეხმარება ბრაუზერს შეტყობინების მიღების დაფიქსირებაში
+self.addEventListener('push', function(event) {
+    console.log('Push message received.');
+});
